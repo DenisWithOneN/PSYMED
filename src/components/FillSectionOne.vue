ee9cec1fa088b27bad8550234d25427f262ce0bf
DenisWithOneN <denis.ursut@emanuel.ro>
2023-12-30 23:44:18 +0200

    fill section one implemented

--- src/components/FillSectionOne.vue
+++ src/components/FillSectionOne.vue
@@ -1,13 +1,15 @@
 <script></script>
 <template>
-  <div class="flex flex-col">
-    <h2 class="text-xl font-bold text-master-grey text-center">De ce sa apelezi la PSYMED?</h2>
-  <p class="text-master-grey text-center">
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident itaque
-    minus, deleniti aspernatur fugiat possimus quos aliquid voluptatem aut
-    consequatur consequuntur fugit in amet accusamus qui, culpa totam eveniet
-    nihil.
-  </p>
+  <div class="flex flex-col w-80 mx-auto">
+    <h2 class="text-xl font-bold text-master-grey text-center">
+      De ce sa apelezi la PSYMED?
+    </h2>
+    <p class="text-master-grey text-center mt-5">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident itaque
+      minus, deleniti aspernatur fugiat possimus quos aliquid voluptatem aut
+      consequatur consequuntur fugit in amet accusamus qui, culpa totam eveniet
+      nihil.
+    </p>
   </div>
 </template>
 <style></style>
