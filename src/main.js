b8b393b91260fc72388c87ea0507faec484b3757
DenisWithOneN <denis.ursut@emanuel.ro>
2024-01-03 14:30:28 +0200

    installed vue google maps plugin

--- src/main.js
+++ src/main.js
@@ -2,4 +2,13 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
+import * as VueGoogleMaps from 'vue2-google-maps'
+Vue.use(VueGoogleMaps, {
+    load: {
+    key: '',
+  },
+    installComponents: false
+})
+
 createApp(App).mount('#app')
+ 
