bcaccadfef0251cca8044b4aa259734c67399c1e
DenisWithOneN <denis.ursut@emanuel.ro>
2024-01-04 20:56:20 +0200

    servicii section responsive for all devices

--- src/components/ServiciiTratamente.vue
+++ src/components/ServiciiTratamente.vue
@@ -11,31 +11,44 @@ export default {
 </script>
 <template>
   <div class="spacer divMintUp"></div>
-  <section class="md:hidden bg-master_mint">
-    <div class="flex flex-col justify-center items-center">
+  <section class="bg-master_mint">
+    <div class="flex flex-col items-center">
       <h2 class="text-4xl text-master_shadow font-bold pt-10">
         Servicii si Tratamente
       </h2>
-      <ServiciiTratamenteCard
-        class="mt-20 mb-20"
-        service="consultatie psihiatrie"
-        description="re eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+      <div class="container mx-auto flex flex-col items-center lg:flex-row lg:justify-evenly lg:pt-14">
+        <div>
+          <ServiciiTratamenteCard
+            class="mt-20 lg:mt-0 mb-20"
+            service="consultatie psihiatrie"
+            description="re eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
       proident, sunt in culpa qui officia deserunt mollit anim id est l"
-      ></ServiciiTratamenteCard>
+          ></ServiciiTratamenteCard>
 
-      <ServiciiTratamenteCard
-        class="mb-20"
-        service="referate medicale"
-        description="re eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+          <ServiciiTratamenteCard
+            class="mb-20"
+            service="referate medicale"
+            description="re eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
       proident, sunt in culpa qui officia deserunt mollit anim id est l"
-      ></ServiciiTratamenteCard>
+          ></ServiciiTratamenteCard>
+        </div>
 
-      <ServiciiTratamenteCard
-        class="mb-20"
-        service="adeverinte medicale"
-        description="re eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+        <div>
+          <ServiciiTratamenteCard
+            class="mb-20"
+            service="adeverinte medicale"
+            description="re eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
       proident, sunt in culpa qui officia deserunt mollit anim id est l"
-      ></ServiciiTratamenteCard>
+          ></ServiciiTratamenteCard>
+          <ServiciiTratamenteCard
+            class="mb-20"
+            service="consiliere psihologica"
+            description="re eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+      proident, sunt in culpa qui officia deserunt mollit anim id est l"
+          >
+          </ServiciiTratamenteCard>
+        </div>
+      </div>
       <FilledButton
         class="mb-20"
         buttonText="programeaza-te"
