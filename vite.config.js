e16ec6fabb62e2fcf7ef726e26face1ede587d2a
DenisWithOneN <denis.ursut@emanuel.ro>
2024-01-04 23:58:21 +0200

    vite config updated

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/your-project-name/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/psymed-website/' : '/',
   plugins: [vue()],
   resolve: {
     alias: {
