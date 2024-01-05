26def5bdd46021893cdfb0c655bf26ff3a5626b9
DenisWithOneN <denis.ursut@emanuel.ro>
2024-01-05 21:39:04 +0200

    added background color props

--- src/components/EmptyButton.vue
+++ src/components/EmptyButton.vue
@@ -1,10 +1,10 @@
 <script setup>
-defineProps(["buttonText", 'borderColor', 'textColor']);
+defineProps(["buttonText", 'borderColor', 'textColor', 'backgroundColor']);
 </script>
 <template>
   <button
-    class="border-2 p-4 rounded-xl font-semibold uppercase"
-    :class="`${borderColor} ${textColor}`"
+    class="border-2 p-4 rounded-xl font-semibold uppercase drop-shadow-3xl "
+    :class="`${borderColor} ${textColor} ${backgroundColor}`"
   >
     {{ buttonText }}
   </button>
