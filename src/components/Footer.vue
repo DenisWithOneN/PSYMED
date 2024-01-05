476ca48348661cee7f77dbcf3d7148a0d50665d9
DenisWithOneN <denis.ursut@emanuel.ro>
2024-01-05 17:35:36 +0200

    updated footer responsiveness and design

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,33 +1,27 @@
 <script></script>
 <template>
-
-<section class="h-72">
-    <div class="background-section text-master_shadow flex flex-col justify-end items-center">
-        <p class="font-bold text-xl mb-5">Cabinete de Psihiatrie Dr. Iova - PSYMED</p>
-        <p>Strada Ciheiului nr.35, bloc A3, parter</p>
-        <p>Oradea, Bihor, Romania</p>
-        <p>(0359) 450 657</p>
-        <div class="w-96 flex justify-between pt-5 underline">
-            <a href="">contact</a>
-            <a href="">programari</a>
-            <a href="">program medici</a>
-            <a href="">locatie</a>
-        </div>
+    <div class="footer spacer"></div>
+    <section class="h-auto bg-master_mint">
+    <div
+      class="background-section text-master_shadow flex flex-col justify-end"
+    >
+      <div class="pl-3">
+        <p class="w-auto font-bold text-md sm:text-xl mb-5 ">
+        Cabinete de Psihiatrie <br> Dr. Iova - PSYMED
+      </p>
+      <p class="text-sm sm:text-base">
+        Strada Ciheiului nr.35, bloc A3, parter
+      </p>
+      <p class="text-sm sm:text-base">Oradea, Bihor, Romania</p>
+      <p class="text-sm sm:text-base">(0359) 450 657</p>
+      </div>
+      <div class="w-atuo flex flex-wrap justify-evenly pt-5 underline sm:w-96">
+        <a href="" class="px-1 text-sm sm:text-base">contact</a>
+        <a href="" class="px-1 text-sm sm:text-base">programari</a>
+        <a href="" class="px-1 text-sm sm:text-base">program medici</a>
+        <a href="" class="px-1 text-sm sm:text-base">locatie</a>
+      </div>
     </div>
-    
-</section>
-
+  </section>
 </template>
-<style scoped>
-
-.background-section {
-  width: auto;
-  height: 100%;
-  position: relative;
-  background-image: url("F:\Universitate\Code\TAW\psymed\src\assets\footer.svg");
-  display: flex;
-  background-repeat: no-repeat;
-  background-size: cover;
-}
-
-</style>
+<style scoped></style>
