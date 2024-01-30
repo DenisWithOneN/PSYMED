c36827e1e00d9a202f3d624a4a6b08096edbfe0a
DenisWithOneN <denis.ursut@emanuel.ro>
2024-01-30 19:06:43 +0200

    removed margin for tablet/desktop view

--- src/components/homepage/NumbersSection.vue
+++ src/components/homepage/NumbersSection.vue
@@ -1,27 +1,25 @@
 <script setup>
-
 import NumbersCard from "@components/homepage/NumbersCard.vue";
-
 </script>
 
 <template>
-    <section class="container mx-auto mt-32 md:flex justify-evenly">
-      <NumbersCard
-        class="my-10"
-        number="400"
-        dataType="pacienti"
-      ></NumbersCard>
-      <NumbersCard
-        class="my-10"
-        number="700"
-        dataType="consultatii de psihiatrie"
-      ></NumbersCard>
-      <NumbersCard
-        class="my-10"
-        number="250"
-        dataType="evaluari psihologice"
-      ></NumbersCard>
-    </section>
+  <li class="container mx-auto md:flex justify-evenly">
+    <NumbersCard
+      class="my-10 md:my-0"
+      number="400"
+      dataType="pacienti"
+    ></NumbersCard>
+    <NumbersCard
+      class="my-10 md:my-0"
+      number="700"
+      dataType="consultatii de psihiatrie"
+    ></NumbersCard>
+    <NumbersCard
+      class="my-10 md:my-0"
+      number="250"
+      dataType="evaluari psihologice"
+    ></NumbersCard>
+  </li>
 </template>
 
 <style></style>
