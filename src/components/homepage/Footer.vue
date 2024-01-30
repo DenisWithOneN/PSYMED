2535687f9b8409260675d686dbb4edfa7e210242
DenisWithOneN <denis.ursut@emanuel.ro>
2024-01-30 21:47:10 +0200

    created new footer design not responsive

--- src/components/homepage/Footer.vue
+++ src/components/homepage/Footer.vue
@@ -1,26 +1,54 @@
 <script></script>
 <template>
-    <div class="footer spacer"></div>
-    <section class="h-auto bg-master_mint">
-    <div
-      class="background-section text-master_shadow flex flex-col justify-end"
-    >
-      <div class="pl-3">
-        <p class="w-auto font-bold text-md sm:text-xl mb-5 ">
-        Cabinete de Psihiatrie <br> Dr. Iova - PSYMED
+  <div class="footer spacer"></div>
+  <section class="h-80 bg-master_mint flex items-center justify-center">
+    <div class="background-section text-master_shadow flex flex-col">
+      <p class="w-auto font-bold text-2xl text-center mb-5">
+        Cabinete de Psihiatrie Dr. Iova - PSYMED
       </p>
-      <p class="text-sm sm:text-base">
-        Strada Ciheiului nr.35, bloc A3, parter
-      </p>
-      <p class="text-sm sm:text-base">Oradea, Bihor, Romania</p>
-      <p class="text-sm sm:text-base">(0359) 450 657</p>
-      </div>
-      <div class="w-atuo flex flex-wrap justify-evenly pt-5 underline sm:w-96">
-        <a href="" class="px-1 text-sm sm:text-base">contact</a>
-        <a href="" class="px-1 text-sm sm:text-base">programari</a>
-        <a href="" class="px-1 text-sm sm:text-base">program medici</a>
-        <a href="" class="px-1 text-sm sm:text-base">locatie</a>
-      </div>
+
+      <ul class="flex">
+        <li class="px-5">
+          <i class="bi bi-heart-fill text-3xl text-orange-700"></i>
+          <p class="font-bold py-2">vreau<br />sa ma programez</p>
+          <p class="font-semibold py-2">
+            viziteaza pagina noastra <br />destinata programarilor
+          </p>
+          <p class="font-semibold py-2 text-orange-700">
+            <a href="">afla mai mult ></a>
+          </p>
+        </li>
+        <li class="px-5">
+          <i class="bi bi-info-square-fill text-3xl text-orange-700"></i>
+          <p class="font-bold py-2">vreau<br />mai multe informatii</p>
+          <p class="font-semibold py-2">
+            viziteaza pagina cu <br />intrebari frecvente
+          </p>
+          <p class="font-semibold py-2 text-orange-700">
+            <a href="">afla mai mult ></a>
+          </p>
+        </li>
+        <li class="px-5">
+          <i class="bi bi-wallet2 text-3xl text-orange-700"></i>
+          <p class="font-bold py-2">vreau sa aflu mai<br />mult despre tarife</p>
+          <p class="font-semibold py-2">
+            viziteaza pagina destinata <br />tarifelor si serviciilor
+          </p>
+          <p class="font-semibold py-2 text-orange-700">
+            <a href="">afla mai mult ></a>
+          </p>
+        </li>
+        <li class="px-5">
+          <i class="bi bi-person-circle text-3xl text-orange-700"></i>
+          <p class="font-bold py-2">vreau sa va<br />contactez</p>
+          <p class="font-semibold py-2">
+            viziteaza pagina de <br />contact
+          </p>
+          <p class="font-semibold py-2 text-orange-700">
+            <a href="">afla mai mult ></a>
+          </p>
+        </li>
+      </ul>
     </div>
   </section>
 </template>
